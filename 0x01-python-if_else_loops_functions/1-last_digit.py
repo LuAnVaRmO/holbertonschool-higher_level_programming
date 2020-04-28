5212c6ca99f2d9bfa61a0813fbfc908740c13624
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-04-28 15:20:24 -0500

    adding less sign

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    l = abs(number) % 10
+    l = (abs(number) % 10)*-1
 else:
     l = number % 10
 if l > 5:
@@ -11,5 +11,5 @@ elif l < 6:
     if l == 0:
         print("Last digit of {:d} is {:d} and is 0".format(number, l))
     else:
-        print("Last digit of {:d} is {:d}".format(number, l) +
+        print("Last digit of {:d} is {:d} ".format(number, l) +
               "and is less than 6 and not 0")
