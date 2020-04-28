12f5f4643e06fbde62c50e3802c127a5da982115
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-04-28 15:15:21 -0500

    fix pep8, the new betty

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    last = abs(number) % 10
+    l = abs(number) % 10
 else:
-    last = number % 10
-if last > 5:
-    msg = "and is greater than 5"
-elif last == 0:
-    msg= "and is 0"
-elif last < 6:
-    msg = "and is less than 6 and not 0"
-print("Last digit of {} is {} {}".format(number, last, msg))
+    l = number % 10
+if l > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5".format(number, l))
+elif l < 6:
+    if l == 0:
+        print("Last digit of {:d} is {:d} and is 0".format(number, l))
+    else:
+        print("Last digit of {:d} is {:d}".format(number, l) +
+              "and is less than 6 and not 0")
