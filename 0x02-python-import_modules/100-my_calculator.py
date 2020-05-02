fa9bd5f922b80aa87c2863f25794f83a48e32247
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-05-01 19:26:00 -0500

    fixing output

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     if op == '+':
         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     elif op == "-":
-        print("{:d} + {:d} = {:d}".format(a, b, sub(a, b)))
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     elif op == '*':
-        print("{:d} + {:d} = {:d}".format(a, b, mul(a, b)))
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     elif op == '/':
-        print("{:d} + {:d} = {:d}".format(a, b, div(a, b)))
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
