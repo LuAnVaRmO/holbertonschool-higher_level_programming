b96f0e8923b432a8b7cdb0f0f7a94bbd57be8415
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-05-05 18:04:02 -0500

    fixing code

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = my_list.copy()
-    for i in new_list:
-        if i % 2 == 0:
+    for i in my_list:
+        if i % 2:
             new_list[i] = True
         else:
             new_list[i] = False
