5c7d6816b750e8ccd336b473d945d5636be66220
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-05-05 16:54:57 -0500

    task 7 fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,5 +4,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a += (0, 0)
     if len(tuple_b) < 2:
         tuple_b += (0, 0)
-    sum = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
+    sum = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return sum
