6331cca64d28980e6f875770aa991311879a50c1
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-05-19 16:46:02 -0500

    fixing docs

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" This module create a Square with a size undefined """
+
+
 class Square:
+    """ atribute private """
     def __init__(self, size=0):
         self.__size = size
