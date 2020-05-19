614e6cfe16a4987544b8a182d2a310ce56c28f74
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-05-19 17:39:22 -0500

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" This module create a Square """
+
+
+class Square:
+    """ attribute private """
+    def __init__(self, size=0):
+        """ Verifying type of value and size"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        """ Function area(size) """
+    def area(self):
+        """ Function return size * size """
+        return self.__size * self.__size
