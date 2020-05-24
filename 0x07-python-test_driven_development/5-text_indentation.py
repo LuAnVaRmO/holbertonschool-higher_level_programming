5143ca58f67c41a2904b1cf7e463c214d83a22e9
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-05-23 23:37:46 -0500

    fixing task 4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
+"""
+text_indentation
+this function put 2 \n before found delimiter[".", "?", ":"]
+text formatted
+"""
+
+
 def text_indentation(text):
+    """
+    :param text: text without format
+    :return: text with format
+    """
     if type(text) != str:
         raise TypeError("text must be a string")
-    current = -1
-    for letter in range(0, len(text)):
-        if text[letter] in [".", "?", ":"]:
-            print(text[current + 1: letter + 1])
-            print()
-            current = letter + 1
+    aux = 0
+    for letter in text:
+        if aux == 1 and letter == ' ':
+            aux = 0
+            pass
+        else:
+            print(letter, end="")
+            aux = 0
+            if letter in [".", "?", ":"]:
+                print("\n")
+                aux = 1
