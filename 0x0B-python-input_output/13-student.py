563143df76500718969eca477aee4d5f6bc20cfe
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-06-03 21:08:04 -0500

    task 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Class Student """
+
+
+class Student:
+    """ Class student """
+    def __init__(self, first_name, last_name, age):
+        """ Init method public attributes """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        newdict = {}
+        if type(attrs) != list:
+            return self.__dict__
+        for i in attrs:
+            try:
+                newdict[i] = self.__dict__[i]
+            except:
+                pass
+        return newdict
+
+    def reload_from_json(self, json):
+        for i in json:
+            try:
+                setattr(self, i, json[i])
+            except:
+                pass
