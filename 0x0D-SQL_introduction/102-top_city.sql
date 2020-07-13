357fdeda791a299b3ea4df07123b66c2d96fecf3
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-07-13 22:41:27 +0000

    advanced tasks

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,5 @@
+-- show top 3 cities temperatures --
+-- MySQL server --
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC LIMIT 3;
