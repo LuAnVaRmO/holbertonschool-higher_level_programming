083c6930a32c5d2e7e542986c5025c90861d1c63
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-08-09 00:08:53 -0500

    more tasks

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -2,21 +2,21 @@
 """ python script """
 
 from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
+
     Session = sessionmaker(engine)
     session = Session()
-    instance = session.query(State).first()
 
-    if instance is None:
-        print("Nothing")
-    else:
-        print("{}: {}".format(instance.id, instance.name))
-
+    st_name = argv[4]
+    state = session.query(State).filter_by(name=st_name).first()
+    print(state.id) if state else print('Not found')
     session.close()
