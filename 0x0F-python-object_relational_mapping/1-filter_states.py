7a83f53d745d22b7e6d87b7f805dfa286af278c1
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-08-08 22:59:41 -0500

    task 1 fixing

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         db=my_db)
 
     cursor = db.cursor()
-    dbase = cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    dbase = cursor.execute("SELECT * FROM states WHERE name LIKE 'N%';")
     res = cursor.fetchall()
     for i in res:
         print(i)
