b2e6d81e05da24380990ec7b209332f0d675316b
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-08-27 11:47:27 -0500

    hotfix shabang

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ Script that fetches a url """
 
 import urllib.request
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ Script that display body of the response"""
 
 from sys import argv
