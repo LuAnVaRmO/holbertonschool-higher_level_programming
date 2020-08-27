83726738e3fb04996cc98ab82bda968be8660561
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-08-27 12:03:19 -0500

    Task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Script that display error msg """
+import urllib.request
+import urllib.parse
+from sys import argv
+import urllib.error
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request(argv[1])
+    try:
+        with urllib.request.urlopen(req) as response:
+            res = response.read()
+        print(res.decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
