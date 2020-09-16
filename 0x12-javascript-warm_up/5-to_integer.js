3c95058e4d65e35a10d6887001e20870af3a2d7f
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-09-15 21:18:45 -0500

    repairing

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,6 +2,8 @@
 const argv = process.argv;
 if (isNaN(argv[2])) {
   console.log('Not a number');
-} else {
+} else if (Number.isInteger(argv[2])) {
   console.log('MY number: ' + parseInt(argv[2]));
+} else {
+  console.log('Not a number');
 }
